86fe658375ecca834762a5875303bc3de1dca81a
futurenowchen <futurenowchen@gmail.com>
2021-05-23 14:17:10 +0800

    first version

---
+++ main.js
@@ -0,0 +1,166 @@
+const BASE_URL = 'https://movie-list.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
+const POSTER_URL = BASE_URL + '/posters/'
+const movies = []
+const dataPanel = document.querySelector('#data-panel')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#search-input')
+const viewChange = document.querySelector('#view-change')
+const MOVIES_PER_PAGE = 12
+let filteredMovies = []
+let viewDisplay = 'fa-th'
+let nowMovieListPage = 1
+
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('.btn-show-movie')) {
+    showMovieModal(event.target.dataset.id)
+  } else if (event.target.matches('.btn-add-favorite')) {
+    addToFavorite(Number(event.target.dataset.id))
+  }
+})
+
+searchForm.addEventListener('submit', function onSearchFormSubmitted(event) {
+  event.preventDefault()
+  const keyword = searchInput.value.trim().toLowerCase()
+
+  filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(keyword)
+  )
+
+  //錯誤處理：無符合條件的結果
+  if (filteredMovies.length === 0) {
+    return alert(`您輸入的關鍵字：${keyword} 沒有符合條件的電影`)
+  }
+
+  //重製分頁器
+  renderPaginator(filteredMovies.length)  //新增這裡
+  //預設顯示第 1 頁的搜尋結果
+  renderMovieList(getMoviesByPage(1))
+})
+
+paginator.addEventListener('click', function onPaginatorClicked(event) {
+  //如果被點擊的不是 a 標籤，結束
+  if (event.target.tagName !== 'A') return
+
+  //透過 dataset 取得被點擊的頁數
+  nowMovieListPage = Number(event.target.dataset.page)
+  //更新畫面
+  renderMovieList(getMoviesByPage(nowMovieListPage))
+})
+
+viewChange.addEventListener('click', function viewChangeClicked(event) {
+  if (event.target.matches('.fa-bars')) {
+    viewDisplay = 'fa-bars'
+    renderPaginator(movies.length)
+    renderMovieList(getMoviesByPage(nowMovieListPage))
+  } else if (event.target.matches('.fa-th')) {
+    viewDisplay = 'fa-th'
+    renderPaginator(movies.length)
+    renderMovieList(getMoviesByPage(nowMovieListPage))
+  }
+})
+
+function renderMovieList(data) {
+  let typeThHTML = ''
+  let typeThStyle = 'row'
+  let typeBarHTML = ''
+  let typeBarStyle = 'flex-cloum'
+
+  data.forEach((item) => {
+    // title, image
+    typeThHTML += `<div class="col-sm-3">
+    <div class="mb-2">
+      <div class="card">
+        <img src="${POSTER_URL + item.image
+      }" class="card-img-top" alt="Movie Poster">
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#movie-modal" data-id="${item.id}">More</button>
+          <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>
+        </div>
+      </div>
+    </div>
+  </div>`
+
+    typeBarHTML += `<div>
+    <div class="mb-2">
+      <div class="card">
+        <div class="card-body flex-row d-flex justify-content-between">
+          <h5 class="card-title">${item.title}</h5>
+          <div class="mr-5">
+          <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#movie-modal" data-id="${item.id}">More</button>
+          <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`
+  })
+  if (viewDisplay === 'fa-th') {
+    dataPanel.className = typeThStyle
+    dataPanel.innerHTML = typeThHTML
+  } else if (viewDisplay === 'fa-bars') {
+    dataPanel.className = typeBarStyle
+    dataPanel.innerHTML = typeBarHTML
+  }
+}
+
+function showMovieModal(id) {
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
+  axios.get(INDEX_URL + id).then((response) => {
+    const data = response.data.results
+    modalTitle.innerText = data.title
+    modalDate.innerText = 'Release date: ' + data.release_date
+    modalDescription.innerText = data.description
+    modalImage.innerHTML = `<img src="${POSTER_URL + data.image
+      }" alt="movie-poster" class="img-fluid">`
+  })
+}
+
+function addToFavorite(id) {
+  const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+  const movie = movies.find((movie) => movie.id === id)
+  if (list.some((movie) => movie.id === id)) {
+    return alert('此電影已經在收藏清單中！')
+  }
+  list.push(movie)
+  localStorage.setItem('favoriteMovies', JSON.stringify(list))
+}
+
+function getMoviesByPage(page) {
+  //計算起始 index 
+  const data = filteredMovies.length ? filteredMovies : movies
+  const startIndex = (page - 1) * MOVIES_PER_PAGE
+  //回傳切割後的新陣列
+  return data.slice(startIndex, startIndex + MOVIES_PER_PAGE)
+}
+
+function renderPaginator(amount) {
+  //計算總頁數
+  const numberOfPages = Math.ceil(amount / MOVIES_PER_PAGE)
+  //製作 template 
+  let rawHTML = ''
+
+  for (let page = 1; page <= numberOfPages; page++) {
+    rawHTML += `<li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>`
+  }
+  //放回 HTML
+  paginator.innerHTML = rawHTML
+}
+
+axios
+  .get(INDEX_URL)
+  .then((response) => {
+    movies.push(...response.data.results)
+    // for (const movie of response.data.results) {
+    //   movies.push(movie)
+    // } 
+    renderPaginator(movies.length)
+    renderMovieList(getMoviesByPage(nowMovieListPage))
+  })
+  .catch((err) => console.log(err))
